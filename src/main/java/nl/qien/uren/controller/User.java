d14453adb839395d3305d3935ad9a0c2059c9a0c
Kalim <kalim.r1989@gmail.com>
2019-08-20 14:24:11 +0200

    user class aangemaakt

---
+++ src/main/java/nl/qien/uren/controller/User.java
@@ -0,0 +1,28 @@
+package nl.qien.uren.controller;
+
+public class User {
+    private String emailAdress;
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    public User(String emailAdress, String password){
+        this.emailAdress = emailAdress;
+        this.password = password;
+    }
+}
