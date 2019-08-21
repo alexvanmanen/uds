c29eb9969606ec08677f0375bd6db6ca13167a95
Kalim <kalim.r1989@gmail.com>
2019-08-21 13:04:15 +0200

    list met allusers in Admin class

--- src/main/java/nl/qien/uren/controller/Admin.java
+++ src/main/java/nl/qien/uren/controller/Admin.java
@@ -1,10 +1,15 @@
 package nl.qien.uren.controller;
 
+import java.util.ArrayList;
+
 public class Admin extends User{
     final int level = 1;
+    public ArrayList <User> allUsers;
+
 
     public User register(String email, String pass){
         User user = new User(email,pass);
+        allUsers.add(user);
         return user;
     }
 
