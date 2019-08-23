a14c69d748d8cdb6794ddb423235e1595eb80f42
Kalim <kalim.r1989@gmail.com>
2019-08-23 13:39:44 +0200

    USER TABLE

---
+++ src/main/java/nl/qien/uren/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package nl.qien.uren.repository;
+
+import nl.qien.uren.controller.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    int count();
+
+    int save(User user);
+
+    Boolean validateUser(String email, String password);
+
+    List<User> findAll();
+}
