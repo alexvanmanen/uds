065f1bdd5e67904586311eb4d8418492794da99b
Kalim <kalim.r1989@gmail.com>
2019-08-23 13:40:37 +0200

    USER TABLE

--- src/main/java/nl/qien/uren/controller/MainController.java
+++ src/main/java/nl/qien/uren/controller/MainController.java
@@ -1,6 +1,8 @@
 package nl.qien.uren.controller;
 
 import nl.qien.uren.repository.EmployeeRepository;
+import nl.qien.uren.repository.JDBCUserRepository;
+import nl.qien.uren.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -23,39 +25,44 @@ public class MainController {
     @Qualifier("JDBCEmployeeRepository")
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/rulezz/{name}")
     public String kalim(@PathVariable String name){
         return "<H1>DIT IS ECHT TOP</H1> <font color='red'>"+name +" rulezzz</font>";
     }
 
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
-        Employee employee = new Employee(new Long(1),"Alex", "van Manen");
-        List<Employee> employees = new ArrayList<>();
-        employees.add(employee);
-        return employees;
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+//        Employee employee = new Employee(new Long(1),"Alex", "van Manen");
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(employee);
+//        return employees;
     }
 
     @GetMapping("/checkPassword/{email}/{password}")
     @ResponseBody
     public boolean checkPassword(@PathVariable String email, @PathVariable String password) {
-        List<User> users = getUsers();
-        for (User a : users) {
-            if(email.equals(a.getEmailAdress()) && password.equals(a.getPassword())){
-                return true;
-            }
-        }
-        return false;
+        return userRepository.validateUser(email, password);
+//        List<User> users = getUsers();
+//        for (User a : users) {
+//            if(email.equals(a.getEmailAdress()) && password.equals(a.getPassword())){
+//                return true;
+//            }
+//        }
+//        return false;
     }
-
-    private List<User> getUsers() {
-        List<User> users = new ArrayList<>();
-        users.add(new User("hallo@hallo.com", "hallo"));
-        users.add(new User("kalim@hallo.com", "hoi"));
-        return users;
-
-
-    }
+//
+//    private List<User> getUsers() {
+//        List<User> users = new ArrayList<>();
+//        users.add(new User(1,"hallo@hallo.com", "hallo"));
+//        users.add(new User(2, "kalim@hallo.com", "hoi"));
+//        return users;
+//
+//
+//    }
     @GetMapping("/count")
     public int getNumberOfEmployees(){
         return employeeRepository.count();
@@ -67,10 +74,10 @@ public class MainController {
 
     }
 
-    @GetMapping("/employee/{id}")
-    public Optional<Employee> getEmployee(@PathVariable Long id){
-        return employeeRepository.findById(id);
-    }
+//    @GetMapping("/employee/{id}")
+//    public Optional<Employee> getEmployee(@PathVariable Long id){
+//        return employeeRepository.findById(id);
+//    }
 
 //    @GetMapping("/createuser/{email}/{password}")
 //    @ResponseBody
