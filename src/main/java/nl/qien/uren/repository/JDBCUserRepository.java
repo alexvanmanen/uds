2912914c81af0bdd1106d90cf2819465a6274b29
Kalim <kalim.r1989@gmail.com>
2019-08-23 13:39:56 +0200

    USER TABLE

---
+++ src/main/java/nl/qien/uren/repository/JDBCUserRepository.java
@@ -0,0 +1,49 @@
+package nl.qien.uren.repository;
+
+import nl.qien.uren.controller.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class JDBCUserRepository implements UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int count(){
+        return jdbcTemplate
+                            .queryForObject("select count(*) from User", Integer.class);
+    }
+
+    @Override
+    public int save(User user){
+        return jdbcTemplate.update("insert into User(ID, email, password), values(?,?,?)", user.getId(), user.getEmailAdress(), user.getPassword());
+
+    }
+
+    @Override
+    public List<User> findAll(){
+
+        return jdbcTemplate.query(
+                "select * from User",
+                (rs, rowNum) ->
+                        new User(
+                                rs.getLong("id"),
+                                rs.getString("emailAdress"),
+                                rs.getString("password")
+                        )
+        );
+    }
+
+    @Override
+    public Boolean validateUser(String email, String password) {
+        String sqlQuery = "select count(*) from User where email=? and password=?";
+        Object[] arrayWithParameters = {email, password};
+        int count = jdbcTemplate.queryForObject(sqlQuery, arrayWithParameters, Integer.class);
+        return count == 1 ? true: false;
+    }
+}
