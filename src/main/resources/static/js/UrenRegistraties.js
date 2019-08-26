4a1565ff8709b2c8153bc45681809beca5a0ddb8
RGCoes <sigma_fi19@yahoo.com>
2019-08-26 11:59:59 +0200

    toevoeging

---
+++ src/main/resources/static/js/UrenRegistraties.js
@@ -0,0 +1,51 @@
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function registreer(){
+    var totaal = 0;
+    var i;
+
+    var a = document.getElementsByClassName("MAAND" );
+    console.log(a.length);
+
+    for( i=0;   i<=30;    i++ ) {
+        var b = a[i].value;
+        var c = Number(b);
+        totaal += c;
+        console.log("value: " + totaal);
+
+    }
+
+    document.getElementById("totaal").value = totaal;
+
+    ajax_get("/urenRegistratie/5/1/" + totaal + "/1993-09-19", function(data){
+        if(data === true) {
+            window.location.assign("./legePagina.html");
+        }
+        else window.location.assign("./legePagina.html")
+    });
+}
+
+
+function doorverwijzen(){
+    window.location.assign("https://www.w3schools.com");
+}
+
+function doorverwijzenHTML(){
+    window.location.assign("./legePagina.html");
+}
