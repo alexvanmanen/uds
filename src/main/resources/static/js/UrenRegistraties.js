e1e47c56150373b4608fb5ad37d1b82abff727cf
RGCoes <sigma_fi19@yahoo.com>
2019-08-26 13:35:09 +0200

    hoi

--- src/main/resources/static/js/UrenRegistraties.js
+++ src/main/resources/static/js/UrenRegistraties.js
@@ -31,7 +31,7 @@ function registreer(){
 
     }
 
-    document.getElementById("totaal").value = totaal;
+    totaal = document.getElementById("totaal").value;
 
     ajax_get("/urenRegistratie/5/1/" + totaal + "/1993-09-19", function(data){
         if(data === true) {
@@ -51,4 +51,15 @@ function doorverwijzenHTML(){
 }
 function methode(){
     window.location.assign("./UrenDeclaratieFormulier.html");
+}
+
+function buildTable(maand){
+    var tabel= "<th>maand</th>";
+    if(maand == "januari"){
+        //alert("hoi");
+        for( var i = 1 ; i < 32 ; i++){
+        tabel += "<tr><td>" + i + " " + maand + "</td><td><input type='number'></td></tr>"
+        }
+        document.getElementById("tabel").innerHTML = tabel;
+    }
 }
