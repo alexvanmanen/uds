267be8e3c7e9e3148213e3ab05d41fd7e878a6b6
Bart Zwaagstra <bartzwaagstra@live.nl>
2019-08-28 20:14:39 +0200

    email toegevoegd aan manageuser.html. TODO json response afwachten (true/false) en daarna de gebruiker doorsturen naar overzicht gebruikers + gelukt niet gelukt.

--- src/main/resources/static/js/email.js
+++ src/main/resources/static/js/email.js
@@ -1,13 +1,17 @@
-function sendEmail(){
+function showEmailForm() {
+    document.getElementById("ajax").style.display = "none";
+    document.getElementById('mailform').style.display='block';
+}
+function sendEmail() {
     var receiver = document.forms["mailform"]["receiver"].value;
     var subject = document.forms["mailform"]["subject"].value;
     var message = document.forms["mailform"]["message"].value;
-        sendMail(receiver, subject, message);
+    sendMail(receiver, subject, message);
 }
 
-function sendMail(receiver, subject, message){
+function sendMail(receiver, subject, message) {
     var object = {
-        "receiver" : receiver,
+        "receiver": receiver,
         "subject": subject,
         "message": message,
     };
