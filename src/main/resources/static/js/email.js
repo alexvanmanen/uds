581b43934f00c0e3a5eea8956fc0a297eb2b516f
Bart Zwaagstra <bartzwaagstra@live.nl>
2019-08-28 14:43:59 +0200

    email html + js gemaakt

---
+++ src/main/resources/static/js/email.js
@@ -0,0 +1,16 @@
+function sendEmail(){
+    var receiver = document.forms["mailform"]["receiver"].value;
+    var subject = document.forms["mailform"]["subject"].value;
+    var message = document.forms["mailform"]["message"].value;
+        sendMail(receiver, subject, message);
+}
+
+function sendMail(receiver, subject, message){
+    var object = {
+        "receiver" : receiver,
+        "subject": subject,
+        "message": message,
+    };
+    var json = JSON.stringify(object);
+    apiPostRequest("/uren/api/v1/sendmail", json);
+}
