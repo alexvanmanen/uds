974d22c3015c6d1ee33a562d77a7e131a642d438
Maartje <maartjeschutte@gmail.com>
2019-09-02 16:17:48 +0200

    timesheet.js cleanup

--- src/main/resources/static/js/timesheet.js
+++ src/main/resources/static/js/timesheet.js
@@ -16,9 +16,3 @@ function timesheetEntry(hoursSpent, dayOftheMonth, entryKind){
     return entry;
 }
 
-
-var entries = [];
-var ts1 = timesheet("2018-02", "OPEN", entries);
-entries.push(timesheetEntry(8,30, "WORK"));
-entries.push(timesheetEntry(8,31, "WORK"));
-var jsonTimesheet = JSON.stringify(ts1);
