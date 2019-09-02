de80196212553f5b71c5eb8d173ed44e4c9deb5f
RGCoes <sigma_fi19@yahoo.com>
2019-09-02 15:03:31 +0200

    for-loops

--- src/main/resources/static/js/UrenRegistraties.js
+++ src/main/resources/static/js/UrenRegistraties.js
@@ -10,20 +10,27 @@ function buildTable(month) {
     document.getElementById("tabel").innerHTML = tabel;
 }
 
-function Registreer(){
-    var object = {
-        "hoursSpent": 800
-    };
-    var json = JSON.stringify(object);
-    apiPostRequest("/uren/api/v1/createTimesheetEntry", json);
-
+function timesheet(yearMonth, state, entries){
     var timesheet = {
-        "state" : "OPEN",
-        "yearMonth": "1993-09"
+        yearMonth : yearMonth,
+        state  : state,
+        entries: entries
     };
+    return timesheet;
+}
 
-    var json2 = JSON.stringify(timesheet);
-    apiPostRequest("/uren/api/v1/createTimesheet", json2)
+function Registreer() {
+    var entries = [];
+    for (var i = 1; i < 32; i++) {
+        entries.push(timesheetEntry(document.getElementById("opdracht" + i).value, 19, "WORK"));
+        entries.push(timesheetEntry(document.getElementById("verlof" + i).value, 19, "LEAVE_OF_ABSENCE"));
+        entries.push(timesheetEntry(document.getElementById("ziekte" + i).value, 19, "ILL"));
+        entries.push(timesheetEntry(document.getElementById("training" + i).value, 19, "TRAINING"));
+        entries.push(timesheetEntry(document.getElementById("overwerk" + i).value, 19, "OVERTIME"));
+    }
+    var ts1 = timesheet("2018-02", "OPEN", entries);
+    var jsonTimesheet = JSON.stringify(ts1);
+    apiPostRequest("/uren/api/v1/createTimesheet", jsonTimesheet);
 }
 
 function apiPostRequest(url, json) {
