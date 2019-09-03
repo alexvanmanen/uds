e7f1dc818edbac9199c78531f7418d1f8220e5b7
Alex van Manen <alexvanmanen@hotmail.com>
2019-09-03 15:14:35 +0200

    basic security added

---
+++ src/main/java/nl/qien/uren/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package nl.qien.uren.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        System.out.println("uitgevoerd----------------------------------");
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("test").roles("USER").build());
+        manager.createUser(users.username("admin").password("test").roles("USER", "ADMIN").build());
+        return manager;
+    }
+
+}
