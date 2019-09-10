c582594e534cf7457177d8fddb5aad27bf7dd45d
RGCoes <sigma_fi19@yahoo.com>
2019-09-10 09:28:06 +0200

    free keygen! click here

---
+++ src/main/java/nl/qien/uren/entity/KeyGenerator.java
@@ -0,0 +1,32 @@
+package nl.qien.uren.entity;
+import java.security.SecureRandom;
+
+public class KeyGenerator {
+    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
+    private static final String NUMBER = "0123456789";
+    private static final String SIGN = "!@#$%^&*";
+
+    private static final String DATA_FOR_RANDOM_STRING = CHAR_LOWER + CHAR_UPPER + NUMBER + SIGN;
+    private static SecureRandom random = new SecureRandom();
+
+    public static void main(String[] args) {
+        System.out.println(generateKey());
+    }
+
+    public static String generateKey() {
+        StringBuilder sb = new StringBuilder(16);
+        for (int i = 0; i < 16; i++) {
+
+            int rndCharAt = random.nextInt(DATA_FOR_RANDOM_STRING.length());
+            char rndChar = DATA_FOR_RANDOM_STRING.charAt(rndCharAt);
+
+            sb.append(rndChar);
+
+        }
+
+        return sb.toString();
+
+    }
+
+}
