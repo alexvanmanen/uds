bc9d0e1117f9bb1bf2d6fbc8fb8e39815c67274b
Alex van Manen <alexvanmanen@hotmail.com>
2019-09-12 10:32:28 +0200

    get month in html

--- src/main/resources/static/js/showTimesheetToCostumer.js
+++ src/main/resources/static/js/showTimesheetToCostumer.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    var id = 8;
-    $.get("/uren/api/v1/getTimeSheet/"+id, function(timesheet, status){
-        $("#yearMonth").text(timesheet.yearMonth.substring(5));
-
+function getMonth(timesheetId){
+    $.get("/uren/api/v1/getTimeSheet/"+timesheetId, function(timesheet, status){
+        $(document).ready(function(){
+               $("#yearMonth").text(timesheet.yearMonth.substring(5));
+        });
     });
-});
+}
