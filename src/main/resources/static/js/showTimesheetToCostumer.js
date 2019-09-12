c28d4bc75fe8d0be18b47b1b190ce2678bd0e064
BenVosse <benvosse@hotmail.com>
2019-09-12 13:36:39 +0200

    baby steps

--- src/main/resources/static/js/showTimesheetToCostumer.js
+++ src/main/resources/static/js/showTimesheetToCostumer.js
@@ -1,7 +1,8 @@
 function getMonth(timesheetId){
     $.get("/uren/api/v1/getTimeSheet/"+timesheetId, function(timesheet, status){
         $(document).ready(function(){
-               $("#yearMonth").text(timesheet.yearMonth.substring(5));
+            $("#month").text(timesheet.yearMonth.substring(5));
+            $("#year").text(timesheet.yearMonth.substring(0,4));
         });
     });
 }
