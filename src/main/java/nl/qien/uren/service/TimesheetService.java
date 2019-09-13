bd7256c75c516fcb5fa9fad727149214a920a0f6
Alex van Manen <alexvanmanen@hotmail.com>
2019-09-13 15:24:25 +0200

    extra comment added

--- src/main/java/nl/qien/uren/service/TimesheetService.java
+++ src/main/java/nl/qien/uren/service/TimesheetService.java
@@ -10,18 +10,35 @@ import java.util.List;
 public class TimesheetService {
 
 
+    /*
+    2. Aanroepen van CreateTimesheetForEmployee. Aanroepen van bovenstaande methode wanneer
+    er een nieuwe gebruiker wordt aangemaakt. De maand = huidige jaar + maand.
+    Employee = aangemaakte gebruiker, status = OPEN.
+     */
+
     public void createTimesheetForEmployee(Employee employee, YearMonth yearMonth, TimesheetState state){
 
     }
 
+    /*
+    3. employeeHasNoTimesheet. Controle methode die controleert of een employee al een timesheet heeft voor deze jaar + maand.
+     */
     private boolean employeeHasNoTimesheet(Employee employee, YearMonth yearMonth){
         return false;
     }
 
+    /*
+    4. getActiveEmployees. Methode aanmaken die lijst van actieve employees ophaalt.
+     */
     private List<Employee> getActiveEmployees(){
         return new ArrayList<Employee>();
     }
 
+    /*
+    5. CreateTimesheets. Voor elke actieve employee moet gecontroleert worden of er geen timesheet is aangemaakt (zie punt 3),
+    dan moet de methode uit stap 1 aangeroepen worden voor de actieve employee.
+    Maand + jaar = huidige jaar en maand, employee = aangemaakt employee en status = open.
+     */
     public void createTimesheets(){
         for(Employee employee: getActiveEmployees()){
             if(employeeHasNoTimesheet(null, null)){
@@ -30,6 +47,9 @@ public class TimesheetService {
         }
     }
 
+    /*
+    7. StartCreationOfTimesheetsEveryMonth. Thread maken die elke 1e dag van de maand de methode uit punt 4 aanroept.
+     */
     public void startCreationOfTimesheetsEveryMonth(){
         createTimesheets();//Vette thread:)
     }
