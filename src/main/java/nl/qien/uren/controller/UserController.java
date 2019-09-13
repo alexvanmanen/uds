cbb869fb365a5abc44f091c3bf2cb4df0735ece8
Alex van Manen <alexvanmanen@hotmail.com>
2019-09-13 10:19:04 +0200

    You now see only your own timesheets as employee

--- src/main/java/nl/qien/uren/controller/UserController.java
+++ src/main/java/nl/qien/uren/controller/UserController.java
@@ -2,7 +2,9 @@ package nl.qien.uren.controller;
 
 import nl.qien.uren.entity.Admin;
 import nl.qien.uren.entity.Timesheet;
+import nl.qien.uren.entity.User;
 import nl.qien.uren.repository.TimesheetRepository;
+import nl.qien.uren.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,9 @@ public class UserController {
 
     @Autowired
     private TimesheetRepository timesheetRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping(value={ "/showTimesheetToCustomer/{customerkey}"}, method = RequestMethod.GET)
     public ModelAndView showTimesheetToCustomer(@PathVariable String customerkey){
@@ -34,8 +39,11 @@ public class UserController {
 
     @GetMapping("/dashboard")
     public String dashboard(@RequestParam(name="name", required=false, defaultValue="wereld") String name, Model model) {
+        User user = userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        System.out.println(user.getId());
         model.addAttribute("name", name);
         model.addAttribute("test", "Bart Zwaagstra");
+        model.addAttribute("userId", user.getId());
         Timesheet timesheet = timesheetRepository.findById(8)
                 .orElseThrow(()-> new RuntimeException("Kan hem niet vinden gap"));
         model.addAttribute("timesheet", timesheet);
